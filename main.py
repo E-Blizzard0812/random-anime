1ad5d029e648d00e7b19fad9fe46eda0061dffdc
E-Blizzard0812 <hossainnaban@gmail.com>
2023-12-21 23:38:17 +0100

    Version 2.2

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#import pandas as pd
+import timeit
+import random
 from random import choice
-import pandas 
 def own():
     return choice(input("Which series ? ").split(","))
 
@@ -7,14 +9,32 @@ def generator():
     list =["Naruto", "Demon Slayer", "JoJo's Bizzare Adventure", "Jujutsu Kaisen", "One Piece", "Fate Series"]
     return choice(list)
 
+def betterg():
+    dict = {
+        "N": "Naruto", 
+        "DS": "Demon Slayer", 
+        "J" : "JoJo's Bizzare Adventure", 
+        "JJK" :"Jujutsu Kaisen", 
+        "OP": "One Piece", 
+        "FS":"Fate Series"       
+    }
+    return random.sample(dict.items(), 1)
+
 def main():
     command = input("Which command? ")
-    if command == "own":
-        return own()
-    elif command == "generator":
-        return generator()
-    else:
-        print("No command found")
+    match command:
+        case "own":
+            return own()
+        case "g": 
+            return generator()
+        case "bg":
+            return betterg()
+        case _:
+            print("No command found")
+
+generator_time = timeit.timeit(lambda: generator(), number = 1000)
+betterg_time = timeit.timeit(lambda: betterg(), number = 1000)
 
 if __name__ == "__main__":
-    print(main())
+    print(main()+f" took {generator_time} seconds")
+    print(main()+f" took {betterg_time} seconds")
