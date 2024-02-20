ef6d988caa9b8669849d3b37e81cf46b527a9c93
E-Blizzard0812 <95707301+E-Blizzard0812@users.noreply.github.com>
2024-02-20 23:52:24 +0100

    Version 2.2.1

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 #import pandas as pd
 import timeit
 import random
-from random import choice
+from random import sample
 def own():
     return choice(input("Which series ? ").split(","))
 
 def generator():
-    list =["Naruto", "Demon Slayer", "JoJo's Bizzare Adventure", "Jujutsu Kaisen", "One Piece", "Fate Series"]
-    return choice(list)
-
-def betterg():
     dict = {
         "N": "Naruto", 
         "DS": "Demon Slayer", 
         "J" : "JoJo's Bizzare Adventure", 
-        "JJK" :"Jujutsu Kaisen", 
+        "JJK" : "Jujutsu Kaisen", 
         "OP": "One Piece", 
-        "FS":"Fate Series"       
+        "FS": "Fate Series",
+        "M" : "Mashle",
+        "COE" : "Classroom of the Elite"       
     }
-    return random.sample(dict.items(), 1)
+    rs =  random.sample(sorted(dict.items()), 1)
+    return rs[0][1]
 
 def main():
     command = input("Which command? ")
@@ -27,14 +26,13 @@ def main():
             return own()
         case "g": 
             return generator()
-        case "bg":
-            return betterg()
         case _:
             print("No command found")
 
-generator_time = timeit.timeit(lambda: generator(), number = 1000)
-betterg_time = timeit.timeit(lambda: betterg(), number = 1000)
+def runtime():
+    generator_time = timeit.timeit(lambda: generator(), number = 1000)
 
 if __name__ == "__main__":
     print(main()+f" took {generator_time} seconds")
-    print(main()+f" took {betterg_time} seconds")
+    #print(main())
+    #print(type(main()))
