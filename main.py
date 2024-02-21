ab04079ef8c75700dae5e3981d31d3c9f6b8fe99
E-Blizzard0812 <95707301+E-Blizzard0812@users.noreply.github.com>
2024-02-21 12:39:05 +0100

    Version 2.2.2

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #import pandas as pd
+#import jikan
 import timeit
 import random
-from random import sample
+
 def own():
     return choice(input("Which series ? ").split(","))
 
@@ -14,10 +15,15 @@ def generator():
         "OP": "One Piece", 
         "FS": "Fate Series",
         "M" : "Mashle",
-        "COE" : "Classroom of the Elite"       
+        "COE" : "Classroom of the Elite",
+        "DAL" : "Date a Live",
+        "" : "" 
     }
-    rs =  random.sample(sorted(dict.items()), 1)
-    return rs[0][1]
+    return (random.sample(sorted(dict.items()), 1))[0][1]
+
+def liste():
+    liste = ["Naruto", "Demon Slayer","JoJo's Bizzare Adventure", "Jujutsu Kaisen", "One Piece", "Fate Series","Mashle","Classroom of the Elite" ]
+    return random.choice(liste)
 
 def main():
     command = input("Which command? ")
@@ -29,10 +35,10 @@ def main():
         case _:
             print("No command found")
 
-def runtime():
-    generator_time = timeit.timeit(lambda: generator(), number = 1000)
+generator_time = timeit.timeit(lambda: generator(), number = 1000)
+liste_time = timeit.timeit(lambda: liste(), number = 1000 )
 
 if __name__ == "__main__":
-    print(main()+f" took {generator_time} seconds")
-    #print(main())
+    #print(main()+f" took {generator_time} seconds and {liste_time} seconds")
+    print(f"You should watch {main()}.")
     #print(type(main()))
